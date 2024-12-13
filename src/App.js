8b5cef7c975b59ae2bea6b083e8e9bb5ce0de0d2
yonatan eshban-laderman <yonlad2@gmail.com>
2024-12-13 12:43:42 -0800

    canceling overflow

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ const App = () => {
       transformOrigin: 'top center',
       height: '111.111vh',
       width: '100%',
-      overflow: 'hidden',
+      overflowX: 'hidden',
       cursor: 'none',
     }}>
       <CalendarHeader onTitleClick={handleHeaderClick} />
